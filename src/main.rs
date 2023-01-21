ce418cd8c95f8d6741b7af111437b2c0f29204d9
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-21 09:58:21 -0500

    starts project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    std::process::exit(decomp());
+}
+
+fn decomp() -> i32 {
+    let args: Vec = std::env::args().collect();
+
+    if args.len() < 2 {
+        println!("Try: {} <filename>", args[0]);
+        return 1;
+    }
+    //get name of file
+    let fname = std::path::Path::new(&*args[1]);
+    //open file
+    let file = fs::File::open(&fname).unwrap();
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+
+        let outpath = match file.enclosed_name() {};
+    }
 }
